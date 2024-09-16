bb6ba15ff3fe9addddcc8e1fe8decbfdf4b84b8f
Srijan Sinha <103751862+Srijandevhub@users.noreply.github.com>
2024-09-16 14:48:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-function getFormvalue(e) {
-    //Write your code here
-	e.preventDefult();
-	var firstName = this.elements['fname'].value;
-    var lastName = this.elements['lname'].value;
+function getFormvalue() {
+    // Get the form element
+    var form = document.getElementById("form1");
+
+    // Get the values of first name and last name from the form
+    var firstName = form.fname.value;
+    var lastName = form.lname.value;
+
+    // Combine first name and last name
+    var fullName = firstName + " " + lastName;
+
+    // Display the full name using an alert
+    alert(fullName);
     
-    // Display the values in an alert
-    alert("First Name: " + firstName + "\nLast Name: " + lastName);
-	return false;
+    // Prevent the form from submitting and refreshing the page
+    // return false;
 }
