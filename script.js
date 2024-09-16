541cfa8d949e35f0dce630a8befbf4c40b409f0a
Srijan Sinha <103751862+Srijandevhub@users.noreply.github.com>
2024-09-16 14:45:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function getFormvalue() {
+function getFormvalue(e) {
     //Write your code here
-
+	e.preventDefult();
+	var firstName = this.elements['fname'].value;
+    var lastName = this.elements['lname'].value;
+    
+    // Display the values in an alert
+    alert("First Name: " + firstName + "\nLast Name: " + lastName);
+	return false;
 }
